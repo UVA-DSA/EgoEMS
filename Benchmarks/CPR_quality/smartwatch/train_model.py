28002f9885048783fded2c0a3cd4f872bfb39afe
sleekEagle <lahirunuwanisme@gmail.com>
2024-10-19 15:27:14 -0500

    train sw cpr model

--- Benchmarks/CPR_quality/smartwatch/train_model.py
+++ Benchmarks/CPR_quality/smartwatch/train_model.py
@@ -10,16 +10,30 @@ import utils
 from Tools.depth_sensor_processing import tools as depth_tools
 
 DATA_FPS=30
-bs=32
+bs=16
 CLIP_LENGTH=5
-EPOCHS=100
-
+EPOCHS=10
+# utils.get_data_stats(data_loader)
+'''
+data stats:
+max depth: 82.0
+min depth: 0.0
+max acc: tensor([69.7335, 59.6060, 77.9001])
+min acc: tensor([-49.3732, -77.9384, -49.8976])
+'''
+MIN_ACC=torch.tensor([-49.3732, -77.9384, -49.8976]).unsqueeze(0).unsqueeze(0)
+MAX_ACC=torch.tensor([69.7335, 59.6060, 77.9001]).unsqueeze(0).unsqueeze(0)
+MIN_DEPTH=0.0
+MAX_DEPTH=82.0
 
 annot_path=r'Annotations/main_annotation.json'
-data_path='C:/Users/lahir/Downloads/data/'
 split_path=r'Annotations/splits/cpr_quality/subject_splits.json'
+log_path=r'Benchmarks/CPR_quality/log.txt'
 
- #in seconds
+#set these paths to your own paths
+model_save_path=r'C:/Users/lahir/Downloads/data/model.pth'
+data_path=r'C:/Users/lahir/Downloads/data/'
+
 
 data = EgoExoEMSDataset(annotation_file=annot_path,
                         data_base_path=data_path,
@@ -47,24 +61,23 @@ model=SWnet.SWNET(in_channels=3,out_len=DATA_FPS*CLIP_LENGTH)
 criterion = torch.nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.5)
+
 rec_loss_meter = utils.AverageMeter('recLoss', ':.4e')
 depth_loss_meter = utils.AverageMeter('depthLoss', ':.4e')
 
-# utils.get_data_stats(data_loader)
-'''
-data stats:
-max depth: 82.0
-min depth: 0.0
-max acc: tensor([69.7335, 59.6060, 77.9001])
-min acc: tensor([-49.3732, -77.9384, -49.8976])
-'''
-MIN_ACC=torch.tensor([-49.3732, -77.9384, -49.8976]).unsqueeze(0).unsqueeze(0)
-MAX_ACC=torch.tensor([69.7335, 59.6060, 77.9001]).unsqueeze(0).unsqueeze(0)
-MIN_DEPTH=0.0
-MAX_DEPTH=82.0
+def init_log(log_path):
+    if os.path.exists(log_path):
+        os.remove(log_path)
+
+def write_log_line(log_path, msg):
+    with open(log_path, 'a') as file:
+        file.write(msg+'\n')
 
 def get_avg_depth(depth_gt):
     avg_depth_list,min_depth_list,n_cpr=[],[],[]
+    if len(depth_gt.shape)==1:
+        depth_gt=depth_gt.unsqueeze(0)
     for i in range(depth_gt.shape[0]):
         p,v=depth_tools.detect_peaks_and_valleys_depth_sensor(depth_gt[i,:].detach().numpy(),show=False)
         peak_depths=depth_gt[i,p]
@@ -100,41 +113,55 @@ def validate(model,data_loader):
         cpr_error=torch.mean((n_cpr-n_cpr_pred)**2)**0.5
         ncpr_error_meter.update(cpr_error.item(),bs)
 
-    print(f'Validation depth loss: {depth_loss_meter.avg:.2f} mm , CPR rate error: {ncpr_error_meter.avg/(DATA_FPS*CLIP_LENGTH)*60:.2f} cpr/min')
+    msg=f'Validation depth loss: {depth_loss_meter.avg:.2f} mm , CPR rate error: {ncpr_error_meter.avg/(DATA_FPS*CLIP_LENGTH)*60:.2f} cpr/min'
+    print(msg)
+    write_log_line(log_path,msg)
 
-for epoch in range(EPOCHS):
-    for i, batch in enumerate(train_data_loader):
-        print(f'Epoch {epoch} , {i}/{len(train_data_loader)} is done',end='\r')
-        data=batch['smartwatch'].float()
-        depth_gt=batch['depth_sensor'].squeeze()
-        depth_gt_mask=depth_gt>0
+init_log(log_path)
+def train():
+    init_log(log_path)
+    for epoch in range(EPOCHS):
+        for i, batch in enumerate(train_data_loader):
+            print(f'Epoch {epoch} , {i}/{len(train_data_loader)} is done',end='\r')
+            data=batch['smartwatch'].float()
+            depth_gt=batch['depth_sensor'].squeeze()
+            depth_gt_mask=depth_gt>0
 
-        #get peaks and valleys from GT depth sensor data
-        avg_depths_list,min_depths_list,_=get_avg_depth(depth_gt)
+            #get peaks and valleys from GT depth sensor data
+            avg_depths_list,min_depths_list,_=get_avg_depth(depth_gt)
 
-        comp_depth=avg_depths_list/MAX_DEPTH
+            comp_depth=avg_depths_list/MAX_DEPTH
 
-        #normnalize depth
-        depth_gt_norm=(depth_gt-min_depths_list.unsqueeze(1))/MAX_DEPTH
+            #normnalize depth
+            depth_gt_norm=(depth_gt-min_depths_list.unsqueeze(1))/MAX_DEPTH
 
-        #normalize acceleration
-        data_norm=(data-MIN_ACC)/(MAX_ACC-MIN_ACC)
+            #normalize acceleration
+            data_norm=(data-MIN_ACC)/(MAX_ACC-MIN_ACC)
 
-        rec,depth_pred=model(data_norm.permute(0,2,1))
-        rec_loss=criterion(rec[depth_gt_mask],depth_gt_norm[depth_gt_mask])
-        d_loss=criterion(depth_pred,comp_depth)
-        loss=rec_loss+d_loss
-        
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+            rec,depth_pred=model(data_norm.permute(0,2,1))
+            rec_loss=criterion(rec[depth_gt_mask],depth_gt_norm[depth_gt_mask])
+            d_loss=criterion(depth_pred,comp_depth)
+            loss=rec_loss+d_loss
+            
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
-        rec_loss_meter.update(rec_loss.item(),bs)
-        depth_loss_meter.update(d_loss.item(),bs)
+            rec_loss_meter.update(rec_loss.item(),bs)
+            depth_loss_meter.update(d_loss.item(),bs)
 
-    print(f'rec loss: {rec_loss_meter.avg} , depth loss: {depth_loss_meter.avg}')
-    if (epoch+1)%1==0:
-        validate(model,valid_data_loader)
+        msg=f'Training Epoch {epoch} , rec loss: {rec_loss_meter.avg} , depth loss: {depth_loss_meter.avg}'
+        print(msg)
+        write_log_line(log_path,msg)
+        scheduler.step()
+
+        if (epoch+1)%1==0:
+            validate(model,valid_data_loader)
+            torch.save(model.state_dict(),model_save_path)
+
+if __name__ == "__main__":
+    train()
+
 
 
 
