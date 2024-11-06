a1267280526081a9562784b489c64358b81f64e2
Keshara Weerasinghe <keshara2032@gmail.com>
2024-11-06 08:34:57 -0500

    add a simple script to copy file specific type preserving dir strucuter over sftp

---
+++ Tools/file_handling/copy_kinect_files_to_localpc.sh
@@ -0,0 +1,5 @@
+# my data
+# find /mnt/standard/NIST\ EMS\ Project\ Data/EgoExoEMS_CVPR2025/Dataset/Final -type f -name "*trimmed_final.mkv" -exec rsync -R {} /media/kesharaw/MIDAS_1 \;
+
+# lahirus data
+ find /mnt/standard/NIST\ EMS\ Project\ Data/EgoExoEMS_CVPR2025/Dataset/Final -type f  -path "*/P*/*" -name "*_trimmed.mkv" -exec rsync -R {} /media/kesharaw/MIDAS_1 \;
